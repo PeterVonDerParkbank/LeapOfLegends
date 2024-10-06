6102ef8eb2fd484aa57d339078573b0217afbcbb
PeterVonDerParkbank <larsmoeller95@yahoo.de>
2024-10-06 22:42:35 +0200

    Changes so it runs on server

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    open: true, // Automatically open the browser
+    port: 5123,
+    host: '127.0.0.1'
   },
 });
