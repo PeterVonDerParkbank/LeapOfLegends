4a4be5550a48de6d903065266ff140c7e9a5406f
PeterVonDerParkbank <larsmoeller95@yahoo.de>
2025-01-03 21:52:07 +0100

    Added new Sprite and changed layout

--- src/Elements/monsterFactory.js
+++ src/Elements/monsterFactory.js
@@ -3,8 +3,8 @@ import Monster from './monster.js';
 export default class MonsterFactory {
     static createMonster(platform) {
         // Position monster on the platform
-        const monsterWidth = 40;
-        const monsterHeight = 40;
+        const monsterWidth = 70;
+        const monsterHeight = 60;
         const x = platform.x + (platform.width - monsterWidth) / 2;
         const y = platform.y - monsterHeight;
         
